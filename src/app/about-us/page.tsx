58a1becfaea39122c57a0808505e39a077c4b67c
honi <dev@erxes.io>
2024-08-29 13:12:12 +0800

    update #4 again

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -5,10 +5,10 @@ import AboutHowToUse from "./how-to-use/page";
 import AboutDiscounts from "./discounts/page";
 import PayemntBasicPage from "@/components/payment/page";
 import WeatherAndCurrency from "@/components/weather-and-currency/page";
-// import { useTranslations } from "next-intl";
+import { useTranslations } from "next-intl";
 
 export default function AboutUs() {
-  // const t = useTranslations("aboutUs");
+  const t = useTranslations("aboutUs");
   return (
     <div className="w-full lg:max-w-screen-2xl h-full flex flex-col items-center lg:gap-[100px] gap-[55px] lg:mt-[60px] lg:mb-[30px] relative">
       <AboutGeregePassport />
